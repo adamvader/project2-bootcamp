e7125147ebe42f9a470f2b131c47dee4ae3ac829
adamvader <adamonglt@hotmail.com>
2022-08-07 14:14:19 +0800

    fix bug for duplicate posting of newsfeed

--- src/index.js
+++ src/index.js
@@ -7,8 +7,4 @@ import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css"; */
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
